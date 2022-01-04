af51c0b5a38dd091cd39f6a30797490ff12e46e1
orchlonn <60744465+orchlonn@users.noreply.github.com>
2022-01-04 11:10:56 +0800

    wip: finished

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const http = require("http");
 const fs = require("fs");
+const urlLib = require("url");
+const path = require("path");
+
 const server = http.createServer((req, res) => {
     res.setHeader("content-type", "text/html");
     const { headers, url, method } = req;
@@ -44,15 +47,47 @@ const server = http.createServer((req, res) => {
             res.statusCode = 200;
             res.write(data);
             res.end();
-        })
+        });
     } else if (url === "/error") {
         fs.readFile("./source/error.html", "utf-8", (error, data) => {
             res.statusCode = 200;
             res.write(data);
             res.end();
-        })
+        });
+    } else if (url.endsWith("jpg") || url.endsWith("png")) {
+        const parsed = urlLib.parse(url);
+        const fileName = path.basename(parsed.pathname);
+        fs.readFile("./source/img/" + fileName, (error, data) => {
+            res.statusCode = 200;
+            res.end(data);
+        });
+    } else if (url.endsWith("pdf")) {
+        const parsed = urlLib.parse(url);
+        const fileName = path.basename(parsed.pathname);
+        fs.readFile("./source/pdf/" + fileName, (error, data) => {
+            res.statusCode = 200;
+            res.setHeader("content-type", "application/pdf")
+            res.end(data);
+        });
+    } else if (url.endsWith(".css")) {
+        const parsed = urlLib.parse(url);
+        const fileName = path.basename(parsed.pathname);
+        fs.readFile("./source/css/" + fileName, (error, data) => {
+            res.statusCode = 200;
+            res.setHeader("content-type", "text/css")
+            res.end(data);
+        });
+    } else if (url.endsWith(".js")) {
+        const parsed = urlLib.parse(url);
+        const fileName = path.basename(parsed.pathname);
+        fs.readFile("./source/js/" + fileName, (error, data) => {
+            res.statusCode = 200;
+            res.setHeader("content-type", "text/javascript")
+            res.end(data);
+        });
     } else {
         res.write("<h1>404 not found</h1>");
+        console.log("-----------", url);
         res.end();
     }
 });
